cb6fcf7ce2b7e8529c5f9341ef3e4dad999e98c9
lawson1000 <lawsonolanrewaju@yahoo.com>
2022-11-08 15:43:34 +0100

    all Errors handled and updated

--- Holiday_api.py
+++ Holiday_api.py
@@ -2,20 +2,52 @@ import requests
 import pandas as pd
 import time
 
-myapi = "7b83d100d7e14c5fbc8705ffe33cbb8d"
+my_api = "7b83d100d7e14c5fbc8705ffe33cbb8d"
 url = "https://holidays.abstractapi.com/v1/?"
 
+
 count = 0
-while count < 4:
+while count < 2:
+
     country = input("which country: ").upper()
-    while country.isalpha() != True or len(country) != 2:
+    while country.isalpha() is not True or len(country) != 2:
         print("Invalid Input\n")
         country = input("Enter a valid alpha 2 Country Code: ").upper()
 
+    while country.isalpha() is True:
+
+        try:
+            # time.sleep(1)
+            # print(requests.get(f"{url}api_key={my_api}&country={country}&year=2022&month=12&day=1").json())
+            time.sleep(1)
+            res = requests.get(f"{url}api_key={my_api}&country={country}&year=2022&month=12&day=1")
+            test = res.json()
+            if type(test) is dict:
+                print(f"No country uses {country}  ")
+                country = input("Enter a valid alpha 2 Country Code: ").upper()
+                while country.isalpha() is not True or len(country) != 2:
+                    print("Invalid Input\n")
+                    country = input("Enter a valid alpha 2 Country Code: ").upper()
+                continue
+            elif type(test) is list:
+                break
+            else:
+                print("unknown error")
+                quit()
+            break
+        except ValueError:
+            print("Enter a Valid Country Code")
+            country = input("Enter a valid alpha 2 Country Code: ").upper()
+        except ConnectionError:
+            print("please Check Your Connection")
+        except Exception:
+            print("Please Check Your Internet and Try again")
+            country = input("which Country: ").upper()
+        break
     while True:
         try:
             year = int(input("Year: "))
-            if year < 1800 or year > 9999:
+            if year < 1800 or year > 19999:
                 print("Invalid! Please Enter between the range of 1800 - 9999\n")
                 continue
             break
@@ -58,8 +90,9 @@ while count < 4:
             print("Invalid! Enter a Numeric value")
 
     while True:
+        day_end = int(input("To which day?: "))
         try:
-            day_end = int(input("To which day?: "))
+            # day_end = int(input("To which day?: "))
             day_end_error = f"Invalid! {month}/{year} do not have day {day_end} \nEnter a valid day\n"
             if day_end < day_start:
                 print("The End date must be greater or equal to the starting Date")
@@ -85,35 +118,46 @@ while count < 4:
         except ValueError:
             print("Invalid! Enter a Numeric value")
 
-    for i in range(day_start, day_end + 1):
-        response = requests.get(f"{url}api_key={myapi}&country={country}&year={year}&month={month}&day={day_start}")
-        holiday = response.json()
-        pairs = []
-        for kp in holiday:
-            pairs = kp
-        if holiday:
-            pair_value = list(pairs.values())
-            pair_keys = list(pairs.keys())
-        else:
-            print(f"There is no holiday on {month}/{day_start}/{year}")
-            day_start += 1
-            time.sleep(1)
-            continue
-        holiday_csv = pd.DataFrame({
-            'Country Info': pair_keys,
-            'Holiday Info': pair_value
-        })
-        holiday_csv_space = pd.DataFrame({
-            "a": [''],
-            "b": ['']
-        })
-        holiday_csv.index = holiday_csv.index + 1
-        print(holiday_csv)
-        if country:
-            holiday_csv.to_csv(f'{country}_holiday_csv_file.csv', mode="a", index=False, header=False)
-            holiday_csv_space.to_csv(f'{country}_holiday_csv_file.csv', sep=" ", mode="a", index=False, header=False)
-        day_start += 1
-        time.sleep(1)
+    run = True
+    while run:
+        for i in range(day_start, day_end + 1):
+            response = requests.get(f"{url}api_key={my_api}&country={country}"
+                                    f"&year={year}&month={month}&day={day_start}")
+            holiday = response.json()
+            # pairs = []
+            # for kp in holiday:
+            #     pairs = kp
+            try:
+                if holiday:
+                    # pair_value = list(pairs.values())
+                    # pair_keys = list(pairs.keys())
+                    holiday_csv = pd.DataFrame(holiday)
 
-    print("------Successfully executed-----")
-    count+=1
+                    # holiday_csv = pd.DataFrame({
+                    #     'Country Info': pair_keys,
+                    #     'Holiday Info': pair_value
+                    # })
+                    holiday_csv_space = pd.DataFrame({
+                        "a": [''],
+                        "b": ['']
+                    })
+                    holiday_csv.index = holiday_csv.index + 1
+                    print(holiday_csv)
+                    if count < 1:
+                        holiday_csv.to_csv(f'{country}_holiday_csv_file.csv', mode="a", index=False, header=False)
+                        holiday_csv_space.to_csv(f'{country}_holiday_csv_file.csv', sep=" ", mode="a", index=False,
+                                                 header=False)
+                        day_start += 1
+                        time.sleep(1)
+                        print("------Executed-----")
+
+                else:
+                    print(f"There is no holiday on {month}/{day_start}/{year}")
+                    day_start += 1
+                    time.sleep(1)
+                    continue
+            except ValueError:
+                print("You must have inserted an invalid information")
+            run = False
+        count += 1
+print("------Successfully executed-----")
